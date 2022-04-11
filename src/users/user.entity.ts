4f334d28069b56dc427331eac0208dd0c073b4dc
Brooks <brooks@pdrutah.com>
2022-04-10 20:38:34 -0600

    added new routes to users controller

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,13 +1,35 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  AfterInsert,
+  AfterUpdate,
+  AfterRemove
+} from 'typeorm'
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
-  id: number;
+  id: number
 
   @Column()
-  email: string;
+  email: string
 
   @Column()
-  password: string;
+  password: string
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted user with id', this.id)
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated user with id', this.id)
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed user with id', this.id)
+  }
 }
