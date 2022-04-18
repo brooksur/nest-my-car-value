52d6ddb9cbed50ea5db9d80db46d3e642970dcf0
Brooks <brooks@pdrutah.com>
2022-04-18 12:33:30 -0600

    added tests for AuthService, setup testing suite for UsersController

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,22 +1,101 @@
-import { Test } from '@nestjs/testing'
+import { Test, TestingModule } from '@nestjs/testing'
+import faker from '@faker-js/faker'
 import { AuthService } from './auth.service'
 import { UsersService } from './users.service'
+import { User } from './user.entity'
 
-it('can create an instance of auth service', async () => {
-  const fakerUsersService = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password })
-  }
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakerUsersService
+describe('AuthService', () => {
+  let service: AuthService, fakeUsersService: Partial<UsersService>
+
+  beforeEach(async () => {
+    const users: User[] = []
+    fakeUsersService = {
+      find: (email: string) => {
+        const filteredUsers = users.filter((user) => user.email === email)
+        return Promise.resolve(filteredUsers)
+      },
+      create: (email: string, password: string) => {
+        const user = {
+          id: Math.round(Math.random() * 1000),
+          email,
+          password
+        } as User
+        users.push(user)
+        return Promise.resolve(user)
       }
-    ]
-  }).compile()
-  const service = module.get(AuthService)
-  expect(service).toBeDefined()
+    }
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService
+        }
+      ]
+    }).compile()
+
+    service = module.get(AuthService)
+  })
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined()
+  })
+
+  it('creates a new user with salted and hashed password', async () => {
+    const email = faker.internet.email()
+    const password = faker.internet.password()
+    const user = await service.signup(email, password)
+    expect(user.password).not.toBe(password)
+    const [salt, hash] = user.password.split('.')
+    expect(salt).toBeDefined()
+    expect(hash).toBeDefined()
+  })
+
+  it('throws an error is user signs up with an email that is already in use', async () => {
+    const email = faker.internet.email()
+    const password = faker.internet.password()
+    await service.signup(email, password)
+    try {
+      await service.signup(email, password)
+      expect(true).toBe(false)
+    } catch (e) {
+      expect(true).toBe(true)
+    }
+  })
+
+  it('throws if signin is called with an unused email', async () => {
+    const email = faker.internet.email()
+    const password = faker.internet.password()
+    try {
+      await service.signup(email, password)
+      expect(true).toBe(false)
+    } catch (e) {
+      expect(true).toBe(true)
+    }
+  })
+
+  it('throws if an invalid password is provided', async () => {
+    const email = faker.internet.email()
+    const password = faker.internet.password()
+    try {
+      await service.signup(email, password)
+      await service.signin(email, password + '@')
+      expect(true).toBe(false)
+    } catch (e) {
+      expect(true).toBe(true)
+    }
+  })
+
+  it('returns a user if correct password is provided', async () => {
+    const email = faker.internet.email()
+    const password = faker.internet.password()
+    await service.signup(email, password)
+    try {
+      const user = await service.signin(email, password)
+      expect(user).toBeTruthy()
+    } catch (e) {
+      expect(false).toBe(true)
+    }
+  })
 })
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,61 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersController } from './users.controller';
+import { Test, TestingModule } from '@nestjs/testing'
+import faker from '@faker-js/faker'
+import { UsersController } from './users.controller'
+import { UsersService } from './users.service'
+import { AuthService } from './auth.service'
+import { User } from './user.entity'
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let controller: UsersController,
+    mockUsersService: Partial<UsersService>,
+    mockAuthService: Partial<AuthService>
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController],
-    }).compile();
+    mockUsersService = {
+      findOne: () => {
+        const user = {
+          id: faker.datatype.number({ max: 10000 }),
+          email: faker.internet.email(),
+          password: faker.internet.password()
+        } as User
+        return Promise.resolve(user)
+      },
+      find: (email: string) => {
+        const user = {
+          id: faker.datatype.number({ max: 10000 }),
+          email: faker.internet.email(),
+          password: faker.internet.password()
+        } as User
+        return Promise.resolve([user])
+      }
+      // remove: () => {
 
-    controller = module.get<UsersController>(UsersController);
-  });
+      // },
+      // update: () => {}
+    }
+    mockAuthService = {
+      // signup: () => {},
+      // signin: () => {}
+    }
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUsersService
+        },
+        {
+          provide: AuthService,
+          useValue: mockAuthService
+        }
+      ],
+      controllers: [UsersController]
+    }).compile()
+
+    controller = module.get<UsersController>(UsersController)
+  })
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+    expect(controller).toBeDefined()
+  })
+})
