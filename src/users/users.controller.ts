66c81753c986218d08a2eb8fe0af51de5d1779b9
Brooks <brooks@pdrutah.com>
2022-04-17 21:20:58 -0600

    added tests

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,7 +8,8 @@ import {
   Query,
   Delete,
   NotFoundException,
-  Session
+  Session,
+  UseGuards
 } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { AuthService } from './auth.service'
@@ -16,6 +17,9 @@ import { CreateUserDto } from './dtos/create-user.dto'
 import { UpdateUserDto } from './dtos/update-user.dto'
 import { Serialize } from '../interceptors/serialize.interceptor'
 import { UserDto } from '../users/dtos/user.dto'
+import { CurrentUser } from './decorators/current-user.decorator'
+import { User } from './user.entity'
+import { AuthGuard } from 'src/guards/auth.guard'
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -26,8 +30,14 @@ export class UsersController {
   ) {}
 
   @Get('/whoami')
-  whoAmI(@Session() session: any) {
-    return this.usersService.findOne(session.userId)
+  @UseGuards(AuthGuard)
+  whoAmI(@CurrentUser() user: User) {
+    return user
+  }
+
+  @Post('/signout')
+  signOut(@Session() session: any) {
+    session.userId = null
   }
 
   @Post('/signup')
